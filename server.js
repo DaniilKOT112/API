7081db151c47b51a9422ba50414be64f9040d5dd
DaniilKOT112 <user1user2user3@mail.ru>
2024-11-15 22:38:01 +0300

    modified:   server.js

--- server.js
+++ server.js
@@ -17,25 +17,41 @@ const pool = new Pool({
     port: 5432,
   });
 
-app.post('/register', async (req, res) => {
-    const {login, password} = req.body;
+  app.post('/register', async (req, res) => {
+    const { login, password } = req.body;
     try {
         const result = await pool.query('SELECT * FROM "user" WHERE login = $1', [login]);
-        
         if (result.rows.length > 0) {
-            return res.status(400).json({error: 'Такой логинг уже существует!'});
+            return res.status(400).json({ error: 'Такой логин уже существует!' });
         }
-        
-        const hasPassword = await bcrypt.hash(password, 10);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
         const insertResult = await pool.query(
-            'INSERT INTO "user" (login, password) VALUES ($1, $2) RETURNING *',
-            [login, hasPassword]
+            'INSERT INTO "user" (login, password, is_profile_complete) VALUES ($1, $2, $3) RETURNING id',
+            [login, hashedPassword, false]
         );
-        res.status(201).json({user: insertResult.rows[0]});
-    }
-    catch (error) {
+        res.status(201).json({ userId: insertResult.rows[0].id });
+    } catch (error) {
         console.error(error);
-        res.status(500).json({error: 'Ошибка регистрации!'});
+        res.status(500).json({ error: 'Ошибка регистрации!' });
+    }
+});
+
+app.post('/create-profile', async (req, res) => {
+    const { login, name, description } = req.body;
+    try {
+        const updateResult = await pool.query(
+            'UPDATE "user" SET name = $1, description = $2, is_profile_complete = $3 WHERE login = $4 RETURNING *',
+            [name, description, true, login]
+        );
+
+        if (updateResult.rowCount === 0) {
+            return res.status(404).json({ error: 'Пользователь не найден!' });
+        }
+        res.status(201).json({ message: 'Профиль успешно создан!' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Ошибка создания профиля!' });
     }
 });
 
